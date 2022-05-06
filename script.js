b7b0120a43e377b0798b152c3cdce455bc079e40
Jonathan Mani <jonathanmani@protonmail.com>
2022-05-06 17:12:46 -0400

    refactoring the project with OOP practices

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-let screen = document.querySelector('.screen');
-let numberButtons = Array.from(document.querySelectorAll('.button'));
+class Calculator {
+    contstuctor(startingValue){
+        this.startingValue = startingValue;
+    }
 
-numberButtons.map(button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'AC':
-                screen.innerText = '';
-                break;
-            case '=':
-                try{
-                    screen.innerText = eval(screen.innerText);
-                } catch{
-                    screen.innerText = 'Error';
-                }
-                break;
-            default:
-                screen.innerText += e.target.innerText;
-        }
+    this.add = function(){
+
+    }
+
+    this.subtract = function(){
+
+    }
+
+    this.multiply = function(){
+
+    }
+
+    this.divide = function(){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let screen = document.querySelector('.screen');
+// let numberButtons = Array.from(document.querySelectorAll('.button'));
+
+// numberButtons.map(button => {
+//     let operand ='';
+//     button.addEventListener('click', (e) => {
+//         switch(e.target.innerText){
+//             case 'AC':
+//                 screen.innerText = '';
+//                 break;
+//             case '=':
+//                 try{
+//                     screen.innerText = Math.evaluate(screen.innerText)
+//                 } catch{
+//                     screen.innerText = 'Error';
+//                 }
+//                 break;
+//             default:
+//                 screen.innerText += e.target.innerText;
+//         }
 
         
-    });
-});
+//     });
+// });
